6c373a50f58c877f4f9b3377cc1d8ba445942d2a
JY16 <JY16@users.noreply.github.com>
2017-10-01 22:13:52 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,89 @@
 // Mobile Navigation
-let toggleMenu = document.querySelectorAll(".menu-toggle")[0];
-toggleMenu.addEventListener("click", menu, false);
-function menu() {
-    document.querySelector("body").classList.toggle("open");
+
+function mobileNav() {
+    let menuToggle = document.querySelector(".menu-toggle");
+    let nav = document.querySelector("nav");
+
+    menuToggle.addEventListener("click", function() {
+        if (nav.style.display === "flex") {
+            nav.style.display = "none";
+        }
+        else {
+            nav.style.display = "flex";
+        }
+    });
 }
 
-// Random Facts Generator
+mobileNav();
 
-let fact = ["I like Triple Chocolate Chunk Cookies.", "I Crochet.", "I like to bake.", "I like programming.",
-    "I fish.", "I like to learn.", "I like reading.", "I like video games (though its been a while since I've played).",
-    "I want to learn C++ or C#.", "I like to be outside."];
-let currentFact = document.querySelector(".facts-p");
-let getFact = document.querySelector(".random-facts button");
 
-function newFact() {
-    let randomNumber = Math.floor(Math.random() * fact.length);
-    currentFact.innerHTML = fact[randomNumber];
+// Show Learning
+
+function showLearning() {
+    let learningLink = document.querySelector(".learning");
+    let learningList = document.querySelector(".learning-list");
+
+    learningLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        if (learningList.style.display === "block") {
+            learningList.style.display = "none";
+        }
+        else {
+            learningList.style.display = "block";
+        }
+    });
 }
 
-getFact.addEventListener("click", newFact);
+showLearning();
+
+// Show the portfolio project
+
+function portfolioProject() {
+    let project = document.querySelectorAll(".project");
+    let portfolioSlider = document.querySelector(".portfolio-slider");
+    let back = document.querySelector(".back");
+    let fullProject = document.querySelector(".full-project-container");
+
+    function showProject() {
+        for (let i = 0; i < project.length; i++) {
+            project[i].addEventListener("click", function() {
+                portfolioSlider.style.left = "-100%";
+                fullProject.style.display = "block";
+            });
+        }
+
+        back.addEventListener("click", function() {
+            portfolioSlider.style.left = 0;
+            fullProject.style.display = "none";
+        });
+    }
+
+    showProject();
+
+    function loadContent() {
+        for (let i = 0; i < project.length; i++) {
+            project[i].addEventListener("click", function (event) {
+                event.preventDefault();
+
+                let xhr = new XMLHttpRequest();
+                let currentProject = this.href;
+
+                xhr.open("GET", currentProject, true);
+                xhr.onload = function() {
+                    if (xhr.status === 200) {
+                        let projectTitle = document.querySelector(".full-project-container h4");
+
+                        projectTitle.innerHTML = document.querySelector(".full-project-container h4").innerHTML = project[i].querySelector(".project p").innerHTML;
+                      
+                        document.querySelector(".project-load").innerHTML = this.responseText;
+                    }
+                };
+                xhr.send();
+            });
+        }
+    }
+
+    loadContent();
+}
+
+portfolioProject();
